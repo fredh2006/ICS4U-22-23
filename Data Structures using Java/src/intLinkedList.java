18b610f8eccc48736b6764174a60265a099138a3
Fred He <fhe@bayviewglen.ca>
2023-01-09 20:12:22 -0500

    updates

---
+++ Data Structures using Java/src/intLinkedList.java
@@ -0,0 +1,68 @@
+public class intLinkedList {
+    private intNode head;
+    private int manyItems;
+
+    public intLinkedList() {
+        this.head = null;
+    }
+
+    public boolean add(Integer data) {
+        intNode temp = new intNode(data);
+
+        if (head == null)
+            head = temp;
+        else {
+            intNode curr = head;
+            while (curr.getLink() != null)
+                curr = curr.getLink();
+            curr.setLink(temp);
+        }
+        return true;
+    }
+
+    public boolean addFront(Integer data) {
+        head = new intNode(data, head);
+        manyItems++;
+        return true;
+    }
+
+    public boolean add(int index, Integer data) {
+        if (index == manyItems)
+            throw new IndexOutOfBoundsException("Index " + index + " is not allowed max index is " + manyItems);
+        if (index == 0) {
+            addFront(data);
+        } else {
+            intNode curr = head;
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.getLink();
+            }
+            curr.setLink(new intNode(data, curr.getLink()));
+        }
+        return true;
+    }
+
+    public boolean isEmpty(){
+        return(head == null);
+    }
+
+    public Integer remove(Integer data){
+        if(head != null && head.getData() == data){
+            head = head.getLink();
+            return data;
+        }else{
+            intNode curr =  head;
+            while(curr.getLink() != null && curr.getLink().getData() != data){
+                curr = curr.getLink();
+            }
+            if(curr.getLink()!=null){
+            curr.setLink(curr.getLink().getLink());
+            return data;
+            }
+            return null;
+        }
+    }
+
+    public int size() {
+        return manyItems;
+    }
+}
