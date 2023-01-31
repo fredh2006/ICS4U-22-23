0e63bb2801f2aec76eebe05a4a32c3edf3faf32f
Fred He <fhe@bayviewglen.ca>
2023-01-31 14:58:29 -0500

    vue starter tasks

---
+++ vue/vue3-intro/creating-vue-app/main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description: 'Hi, my name is Fred. I love wearing socks on my hands'
+        }
+    }
+});
